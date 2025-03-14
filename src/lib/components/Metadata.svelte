7e9a0892bd6ed573a06cf0b7f665312aea751a16
Brian Lee <brianlee.lck@gmail.com>
2025-03-14 15:55:47 +0000

    Update description typo

--- src/lib/components/Metadata.svelte
+++ src/lib/components/Metadata.svelte
@@ -7,6 +7,6 @@
     <title>{title}</title>
     <meta name="description" content={description} />
     <meta property="og:title" content={title} />
-    <meta property="og:description" content={title} />
+    <meta property="og:description" content={description} />
     <meta property="og:image" content="/assets/photo.jpg" />
 </svelte:head>
