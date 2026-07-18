c63784ec48ec92c3ec7010f17e89678d750fa566
Lee Cheuk-Kit <51740145+brianlck@users.noreply.github.com>
2026-07-18 02:27:12 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,12 +10,13 @@
 </div>
 
 <div class="layout font-light space-y-14 text-lg">
-	<div class="space-y-5">
-		<p> I currently work at Jane Street. </p>
-		<p> Previously, I was a researcher at Harvard and Cambridge, where I study the mathematical characterisation of dynamical systems in generative modeling and the natural sciences. </p>
-		<p> Once upon a time, I did an undergraduate degree at the University of Cambridge too! </p>
-		<p> Feel free to reach out at <a class="link" href="mailto:brianlee.lck@gmail.com">brianlee.lck@gmail.com</a>. </p>
-	</div>
+    <div class="space-y-5">
+        <p> I am working on a new research lab. </p>
+        <p> Prior to this, I was at Harvard working on the theory of generative modeling, following my undergraduate studies at Cambridge. </p>
+        <p> I also spent a year working in Hong Kong, my hometown, working at Jane Street. </p>
+        <p> Depending on the time of the year, I can be found in Hong Kong, London, or San Francisco. </p>
+        <p> Feel free to reach out at <a class="link" href="mailto:brianlee.lck@gmail.com">brianlee.lck@gmail.com</a>. </p>
+    </div>
 
 	<div class="space-y-5">
 		<img alt="Brian Lee sitting on a bench in Richmond" src="/assets/photo.jpg" />
