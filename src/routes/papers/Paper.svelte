809355e177cd058251d8226c3341a74aaf511573
Lee Cheuk-Kit <51740145+brianlck@users.noreply.github.com>
2026-08-17 00:56:31 +0100

    fix spacing and things

--- src/routes/papers/Paper.svelte
+++ src/routes/papers/Paper.svelte
@@ -15,24 +15,25 @@
 </script>
 
 <div class="space-y-4">
-  <h3 class="mb-2 text-xl font-semibold text-black flex items-center gap-2">
-    <a class="link mr-1" href={data.link}>{data.title}</a>
-    <small class="text-base font-normal whitespace-nowrap text-neutral-500">
+  <h3 class="mb-2 flex items-start justify-between gap-4 text-xl font-semibold text-black">
+    <a class="link min-w-0" href={data.link}>
+      {data.title}
+    </a>
+
+    <small class="shrink-0 whitespace-nowrap text-base font-normal text-neutral-500">
       {data.date}
     </small>
   </h3>
 
   <p class="font-serif">
     <span class="g">With </span>
+
     {#each data.authors as author, i}
-      {#if i < data.authors.length - 2}
-        {author}<span class="g">,&nbsp;</span>
-      {:else if i === data.authors.length - 2}
-        {author}<span class="g">,&nbsp;and&nbsp;</span>
-      {:else}
-        {author}
-      {/if}
+      <span class="whitespace-nowrap">
+        {author}{#if i < data.authors.length - 2}<span class="g">,&nbsp;</span>{:else if i === data.authors.length - 2}<span class="g">,&nbsp;and&nbsp;</span>{/if}
+      </span>
     {/each}
   </p>
+
   <hr />
 </div>
