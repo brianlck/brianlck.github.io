00a95c283ee07d8b80d00112d24dfdc25955a0b1
Lee Cheuk-Kit <51740145+brianlck@users.noreply.github.com>
2026-08-17 01:04:21 +0100

    fix 2

--- src/routes/papers/Paper.svelte
+++ src/routes/papers/Paper.svelte
@@ -31,12 +31,18 @@
 
   <p class="font-serif">
     <span class="g">With </span>    
-    {#each data.authors as author, i}
+     {#each data.authors as author, i}
       {#if i < data.authors.length - 2}
-        <span class="whitespace-nowrap">{author},</span>{' '}
+        <span class="whitespace-nowrap">
+          {author}<span class="g">,</span>
+        </span>{' '}
       {:else if i === data.authors.length - 2}
-        <span class="whitespace-nowrap">{author},</span>{' '}
-        <span class="whitespace-nowrap">and&nbsp;{data.authors[i + 1]}</span>
+        <span class="whitespace-nowrap">
+          {author}<span class="g">,</span>
+        </span>{' '}
+        <span class="whitespace-nowrap">
+          <span class="g">and&nbsp;</span>{data.authors[i + 1]}
+        </span>
       {/if}
     {/each}
   </p>
